4bb57c10be6c0c8345cda256d2ee99a4774d0a46
Johan Beaucé <johan.beauce@gmail.com>
2025-01-01 15:23:25 +0100

    Use JavaFaker to create a random gift

--- exercise/java/day03/src/test/java/preparation/SantaWorkshopServiceTest.java
+++ exercise/java/day03/src/test/java/preparation/SantaWorkshopServiceTest.java
@@ -1,55 +1,83 @@
 package preparation;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Long.MAX_VALUE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName("Given a SantaWorkshopService")
 class SantaWorkshopServiceTest {
     private static final String RECOMMENDED_AGE = "recommendedAge";
     private SantaWorkshopService service;
+    private Faker faker;
+    private String giftName;
+    private double weight;
+    private String color;
+    private String material;
 
     @BeforeEach
     void setUp() {
         service = new SantaWorkshopService();
+        faker = new Faker();
     }
 
-    @Test
-    void prepareGiftWithValidToyShouldInstantiateIt() {
-        var giftName = "Bitzee";
-        double weight = 3;
-        var color = "Purple";
-        var material = "Plastic";
+    @Nested
+    class Given_a_random_gift {
 
-        var gift = service.prepareGift(giftName, weight, color, material);
+        @BeforeEach
+        void setUp() {
+            giftName = faker.commerce().productName();
+            color = faker.color().name();
+            material = faker.options().option("Cotton", "Wood", "Metal");
+        }
 
-        assertThat(gift).isNotNull();
-    }
+        @Nested
+        class Weight_under_5 {
 
-    @Test
-    void retrieveAttributeOnGift() {
-        var giftName = "Furby";
-        double weight = 1;
-        var color = "Multi";
-        var material = "Cotton";
+            @BeforeEach
+            void setUp() {
+                weight = faker.number().randomDouble(3, 0, 5);
+            }
 
-        var gift = service.prepareGift(giftName, weight, color, material);
-        gift.addAttribute(RECOMMENDED_AGE, "3");
+            @Test
+            void prepare_gift_with_valid_toy_should_instantiate_it() {
+                var gift = service.prepareGift(giftName, weight, color, material);
 
-        assertThat(gift.getRecommendedAge())
-                .isEqualTo(3);
-    }
+                assertThat(gift)
+                        .isNotNull();
+            }
 
-    @Test
-    void failsForATooHeavyGift() {
-        var giftName = "Dog-E";
-        double weight = 6;
-        var color = "White";
-        var material = "Metal";
+            @Test
+            void retrieve_attribute_on_gift() {
+                var gift = service.prepareGift(giftName, weight, color, material);
+                gift.addAttribute(RECOMMENDED_AGE, "3");
 
-        assertThatThrownBy(() -> service.prepareGift(giftName, weight, color, material))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Gift is too heavy for Santa's sleigh");
+                assertThat(gift.getRecommendedAge())
+                        .isEqualTo(3);
+            }
+        }
+
+        @Nested
+        class Weight_over_5 {
+            @BeforeEach
+            void setUp() {
+                weight = faker.number().randomDouble(3, 6, MAX_VALUE);
+            }
+
+            @Test
+            void fails_for_a_too_heavy_gift() {
+                assertThatThrownBy(() -> service.prepareGift(giftName, weight, color, material))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("Gift is too heavy for Santa's sleigh");
+            }
+        }
     }
 }
