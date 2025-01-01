64a58c9e566c9153f944daa868ab90d5190bc4db
Johan Beaucé <johan.beauce@gmail.com>
2025-01-01 23:27:21 +0100

    Add test with Mockito and Test Double

--- exercise/java/day04/src/test/java/routine/RoutineTests.java
+++ exercise/java/day04/src/test/java/routine/RoutineTests.java
@@ -1,15 +1,99 @@
 package routine;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 
 class RoutineTests {
-    @Test
-    void startRoutineWithMockito() {
-        // Write a test using Mockito
+
+    private EmailService emailService;
+    private ScheduleService scheduleService;
+    private ReindeerFeeder reindeerFeeder;
+
+    private Routine routine;
+
+    @Nested
+    class Given_mocks {
+
+        @BeforeEach
+        void setUp() {
+            emailService = mock(EmailService.class);
+            scheduleService = mock(ScheduleService.class);
+            reindeerFeeder = mock(ReindeerFeeder.class);
+        }
+
+        @Test
+        void startRoutineWithMockito() {
+            routine = new Routine(emailService, scheduleService, reindeerFeeder);
+            routine.start();
+
+            InOrder inOrder = Mockito.inOrder(scheduleService, reindeerFeeder, emailService);
+            inOrder.verify(scheduleService).todaySchedule();
+            inOrder.verify(scheduleService).organizeMyDay(any());
+            inOrder.verify(reindeerFeeder).feedReindeers();
+            inOrder.verify(emailService).readNewEmails();
+            inOrder.verify(scheduleService).continueDay();
+        }
     }
 
-    @Test
-    void startRoutineWithManualTestDoubles() {
-        // Write a test using your own test double(s)
+    @Nested
+    class Given_manual_test_doubles {
+
+        private boolean isEmailServiceReadNewEmailsCalled;
+        private boolean isScheduleServiceTodayScheduleCalled;
+        private boolean isScheduleServiceOrganizeMyDayCalled;
+        private boolean isScheduleServiceContinueDayCalled;
+        private boolean isReindeerFeederFeedReindeersCalled;
+
+        @BeforeEach
+        void setUp() {
+            emailService = new EmailService() {
+                @Override
+                public void readNewEmails() {
+                    isEmailServiceReadNewEmailsCalled = true;
+                }
+            };
+            scheduleService = new ScheduleService() {
+                @Override
+                public Schedule todaySchedule() {
+                    isScheduleServiceTodayScheduleCalled = true;
+                    return new Schedule();
+                }
+
+                @Override
+                public void organizeMyDay(Schedule schedule) {
+                    isScheduleServiceOrganizeMyDayCalled = true;
+                }
+
+                @Override
+                public void continueDay() {
+                    isScheduleServiceContinueDayCalled = true;
+                }
+            };
+            reindeerFeeder = new ReindeerFeeder() {
+                @Override
+                public void feedReindeers() {
+                    isReindeerFeederFeedReindeersCalled = true;
+                }
+            };
+        }
+
+        @Test
+        void startRoutineWithManualTestDoubles() {
+            routine = new Routine(emailService, scheduleService, reindeerFeeder);
+            routine.start();
+
+            assertThat(isScheduleServiceTodayScheduleCalled).isTrue();
+            assertThat(isScheduleServiceOrganizeMyDayCalled).isTrue();
+            assertThat(isReindeerFeederFeedReindeersCalled).isTrue();
+            assertThat(isEmailServiceReadNewEmailsCalled).isTrue();
+            assertThat(isScheduleServiceContinueDayCalled).isTrue();
+        }
     }
 }
